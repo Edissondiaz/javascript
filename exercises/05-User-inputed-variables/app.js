f4e0086ca445b3b92d8057757a616159ba152a58
Edissondiaz <edissondiaz123@gmail.com>
2021-09-21 01:01:48 +0000

    guardado

--- exercises/05-User-inputed-variables/app.js
+++ exercises/05-User-inputed-variables/app.js
@@ -1,3 +1,3 @@
-var age = prompt('What is your age?');
+var age = prompt(What is your age?);
 
 // Your code below:
--- exercises/11-Create-a-new-function/app.js
+++ exercises/11-Create-a-new-function/app.js
@@ -1,3 +1,8 @@
 /* This exercise is almost identical to 15-Random numbers - consider merging them here and using another concept for 15? */
+ Math.random generateRandom(a) {
+    
+    return a
 
+}
 // Declare your function here
+console.log(generateRandom);
